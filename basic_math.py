2b31f09f4576bf15ca406fda9785645475649f14
vivbkanands <vivbkanand.s@lifecell.in>
2025-12-18 10:53:05 +0530

    Basic Math

---
+++ basic_math.py
@@ -0,0 +1,8 @@
+firstInput = int(input('Enter the first number: '))
+secondInput = int(input('Enter the second number: '))
+
+
+print('Addition: ', firstInput+secondInput)
+print('Subtraction: ', firstInput-secondInput)
+print('Multiplication: ', firstInput*secondInput)
+print('Division: ', firstInput/secondInput)
